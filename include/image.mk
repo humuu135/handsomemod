2ece2e688bb44acbec3b704b1927fa326bc74bf3
humuu135 <208154532+humuu135@users.noreply.github.com>
2025-06-05 20:31:24 +0900

    [my][e2fsprogs][backport]resize2fsがうまく動かないのでバックポート

--- include/image.mk
+++ include/image.mk
@@ -43,6 +43,10 @@ IMG_COMBINED:=$(IMG_PREFIX)-combined
 IMG_PART_SIGNATURE:=$(shell echo $(SOURCE_DATE_EPOCH)$(LINUX_VERMAGIC) | mkhash md5 | cut -b1-8)
 IMG_PART_DISKGUID:=$(shell echo $(SOURCE_DATE_EPOCH)$(LINUX_VERMAGIC) | mkhash md5 | sed -E 's/(.{8})(.{4})(.{4})(.{4})(.{10})../\1-\2-\3-\4-\500/')
 
+EXT4_UUID_NAMESPACE:=e67fcece-4ba7-4341-a2ed-d366699c745b
+EXT4_ROOTFS_UUID:=$(shell uuidgen -s -n $(EXT4_UUID_NAMESPACE) -N rootfs-uuid-$(SOURCE_DATE_EPOCH))
+EXT4_ROOTFS_HASH_SEED:=$(shell uuidgen -s -n $(EXT4_UUID_NAMESPACE) -N rootfs-hash-seed-$(SOURCE_DATE_EPOCH))
+
 MKFS_DEVTABLE_OPT := -D $(INCLUDE_DIR)/device_table.txt
 
 ifneq ($(CONFIG_BIG_ENDIAN),)
@@ -261,12 +265,15 @@ define Image/mkfs/ubifs
 endef
 
 define Image/mkfs/ext4
-	$(STAGING_DIR_HOST)/bin/make_ext4fs -L rootfs \
-		-l $(ROOTFS_PARTSIZE) -b $(CONFIG_TARGET_EXT4_BLOCKSIZE) \
+	$(if $(SOURCE_DATE_EPOCH),E2FSPROGS_FAKE_TIME=$(SOURCE_DATE_EPOCH)) \
+		$(STAGING_DIR_HOST)/bin/mkfs.ext4 -L rootfs \
+		-T default -b $(CONFIG_TARGET_EXT4_BLOCKSIZE) \
 		$(if $(CONFIG_TARGET_EXT4_RESERVED_PCT),-m $(CONFIG_TARGET_EXT4_RESERVED_PCT)) \
-		$(if $(CONFIG_TARGET_EXT4_JOURNAL),,-J) \
-		$(if $(SOURCE_DATE_EPOCH),-T $(SOURCE_DATE_EPOCH)) \
-		$@ $(call mkfs_target_dir,$(1))/
+		$(if $(CONFIG_TARGET_EXT4_JOURNAL),,-O ^has_journal) \
+		-U $(EXT4_ROOTFS_UUID) -E hash_seed=$(EXT4_ROOTFS_HASH_SEED) \
+		-u 0:0 \
+		-d $(call mkfs_target_dir,$(1))/ \
+		$@ $$(($(ROOTFS_PARTSIZE)/$(CONFIG_TARGET_EXT4_BLOCKSIZE)))
 endef
 
 define Image/Manifest
